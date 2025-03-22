e84e248eb4eae746e0c07fa40480ca342e7bd173
Your Name <your.email@example.com>
2025-03-21 22:10:55 -0300

    Revert to React 17 render method

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./styles/tailwind.css";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
